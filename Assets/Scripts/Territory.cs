3dfc5760599191d1a4ef9d02a9a16f21e3580874
michi0987 <michi0987@gmail.com>
2019-11-05 22:22:19 +0100

    Init

---
+++ Assets/Scripts/Territory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PolygonCollider2D))]
+public class Territory : MonoBehaviour
+{
+
+    Color32 hoverColor = new Color32(255, 0, 0, 255);
+
+    private void OnMouseEnter()
+    {
+        GetComponent<SpriteRenderer>().color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<SpriteRenderer>().color = new Color32(255, 255, 255, 0);
+    }
+}
