df8f1f68287d9930d2fd24de89743188426e41a5
ravvv666 <ravvv666@gmail.com>
2019-11-20 00:21:58 +0100

    Dodany interfejs do wyboru koloru gracza(bez logiki)

---
+++ Assets/Scripts/RGBSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RGBSlider : MonoBehaviour
+{
+    private TextMeshProUGUI TextPro;
+
+    void Start() {
+        TextPro = GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    public void SetSliderValue(float sliderValue) {
+        TextPro.text = Mathf.Round(sliderValue).ToString();
+    }
+}
