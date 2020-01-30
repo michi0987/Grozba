8a119fdc2f6bdba8b1ecaa64587e5a99b2237fe6
kelo0 <57834911+kelo0@users.noreply.github.com>
2020-01-31 00:17:45 +0100

    ResourceTag

---
+++ Assets/Scripts/ResourceTagController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceTagController : MonoBehaviour
+{
+    TMPro.TextMeshPro mText;
+
+    private void Start()
+    {
+        mText = this.gameObject.GetComponentInChildren<TMPro.TextMeshPro>();
+    }
+
+    public void setNumber(int number)
+    {
+        string text = number.ToString();
+        mText.SetText(text);
+    }
+
+    public int getNumber()
+    {
+        return int.Parse(mText.text);
+    }
+}
